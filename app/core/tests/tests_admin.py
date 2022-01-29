2192a8ec2700db25e0001cd900469a16594c0574
Ryan de Villiers <ryandv62@gmail.com>
2022-01-29 15:56:14 +0200

    Modify Django admin to support the creation of users

--- app/core/tests/tests_admin.py
+++ app/core/tests/tests_admin.py
@@ -1,9 +1,11 @@
 from django.test import TestCase, Client
 from django.contrib.auth import get_user_model
-from django.urls import reverse # allows us to generate urls from admin page
+from django.urls import reverse  # allows us to generate urls from admin page
+
 
 class AdminSiteTests(TestCase):
-# Set Up
+
+    # Set Up
     def setUp(self):
         self.client = Client()
         self.admin_user = get_user_model().objects.create_superuser(
@@ -17,22 +19,26 @@ class AdminSiteTests(TestCase):
             name='Test User Full Name'
         )
 
-
     def test_for_users_listed(self):
         """Test that users are listed on user page"""
         # Dynamically updates, instead of hardcoding
-        url = reverse('admin:core_user_changelist') 
-        
+        url = reverse('admin:core_user_changelist')
         res = self.client.get(url)
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
-    
+
     def test_user_change_page(self):
         """Test that the user edit page works"""
-        url = reverse('admin:core_user_change',args=[self.user.id])
+        url = reverse('admin:core_user_change', args=[self.user.id])
         res = self.client.get(url)
 
         self.assertEqual(res.status_code, 200)
 
+    def test_create_user_page(self):
+        """Test that the create user page works"""
 
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
