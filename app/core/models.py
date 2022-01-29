2660f97728de38444c9ab165480d39fbaac478f9
Ryan de Villiers <ryandv62@gmail.com>
2022-01-29 12:17:00 +0200

    Add support for creating superusers
    Check flask8 compliant

--- app/core/models.py
+++ app/core/models.py
@@ -2,10 +2,9 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
                                         PermissionsMixin
 
-# **extra_fields automatically extends function if new user parameters are added
 
 class UserManager(BaseUserManager):
-    
+
     def create_user(self, email, password=None, **extra_fields):
         """Creates and saves a new user"""
         if not email:
@@ -16,15 +15,23 @@ class UserManager(BaseUserManager):
 
         return user
 
+    def create_superuser(self, email, password):
+        """Creates and saves a new superuser"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+
 class User(AbstractBaseUser, PermissionsMixin):
     """Custom user model that supports using email instead of username"""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-
     objects = UserManager()
 
 # Customise username email form username to email
     USERNAME_FIELD = 'email'
-
--- app/core/tests/tests_models.py
+++ app/core/tests/tests_models.py
@@ -10,8 +10,8 @@ class ModelTests(TestCase):
         password = "TestPass123"
         # calling create user function on the user manager for our user model
         user = get_user_model().objects.create_user(
-            email = email,
-            password = password
+            email=email,
+            password=password
             )
 
         self.assertEqual(user.email, email)
@@ -23,7 +23,7 @@ class ModelTests(TestCase):
     def test_new_user_email_normalised(self):
         """Test the email for a new user us normalised"""
         email = 'test@VUMATEL.COM'
-        user = get_user_model().objects.create_user(email,'test123')
+        user = get_user_model().objects.create_user(email, 'test123')
 
         self.assertEqual(user.email, email.lower())
 
@@ -31,3 +31,12 @@ class ModelTests(TestCase):
         """Test creating email with no email raises error"""
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user(None, 'test123')
+
+    def test_create_new_superuser(self):
+        """Test creating a new superuser"""
+        user = get_user_model().objects.create_superuser(
+            'test@vumatel.com',
+            'test123'
+            )
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
