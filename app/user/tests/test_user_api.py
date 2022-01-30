643c03c3a4a1612ba391bc7bcf3a999b2926d87c
Ryan de Villiers <ryandv62@gmail.com>
2022-01-30 16:38:24 +0200

    Add Manage User endpoint

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -8,12 +8,14 @@ from rest_framework import status
 # URLS to use to generate HTTP post request
 CREATE_USER_URL = reverse('user:create')
 TOKEN_URL = reverse('user:token')
+ME_URL = reverse('user:me')
+
 
 def create_user(**params):
     return get_user_model().objects.create_user(**params)
 
 
-class PublicUserApiTests(TestCase):  # Unauthenticated
+class PublicUserApiTests(TestCase):  # Unauthenticated users
     """Test the users API (public)"""
 
     def setUp(self):
@@ -42,7 +44,7 @@ class PublicUserApiTests(TestCase):  # Unauthenticated
             }
         create_user(**payload)
 
-        res =self.client.post(CREATE_USER_URL, payload)
+        res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
@@ -62,9 +64,7 @@ class PublicUserApiTests(TestCase):  # Unauthenticated
 
     def test_create_token_for_user(self):
         """Test that a token is created for the user"""
-        payload = {'email': 'ryantest@vumatel.com',
-                    'password': 'testpass'
-                    }
+        payload = {'email': 'ryantest@vumatel.com', 'password': 'testpass'}
         create_user(**payload)
         res = self.client.post(TOKEN_URL, payload)
 
@@ -86,8 +86,7 @@ class PublicUserApiTests(TestCase):  # Unauthenticated
 
     def test_create_token_no_user(self):
         """Test that token is not created if user doesn't exist"""
-        payload = {'email': 'ryantest@vumatel.com',
-                    'password': 'testpass'}
+        payload = {'email': 'ryantest@vumatel.com', 'password': 'testpass'}
         res = self.client.post(TOKEN_URL, payload)
 
         self.assertNotIn('token', res.data)
@@ -99,3 +98,49 @@ class PublicUserApiTests(TestCase):  # Unauthenticated
 
         self.assertNotIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_retrieve_user_unathorised(self):
+        """Authentication is required for users"""
+        res = self.client.get(ME_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateUsersApiTests(TestCase):
+    """Tests API requests that require authentication"""
+    # Run uthentication process
+    def setUp(self):
+        self.user = create_user(
+            email='ryantest@vumatel.com',
+            password='testpass',
+            name='Test Name'
+        )
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+
+    def test_retrieve_profile_success(self):
+        """Test retrieving profile for logged in user"""
+        res = self.client.get(ME_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, {
+            'name': self.user.name,
+            'email': self.user.email
+        })
+
+    def test_post_me_not_allowed(self):
+        """Test that post is not allowed on the me url"""
+        res = self.client.post(ME_URL, {})
+
+        self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_update_user_profile(self):
+        """Test updating the user profile for authenticated user"""
+        payload = {'name': 'new name', 'password': 'newpass123'}
+        res = self.client.patch(ME_URL, payload)
+
+        self.user.refresh_from_db()
+
+        self.assertEqual(self.user.name, payload['name'])
+        self.assertTrue(self.user.check_password(payload['password']))
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
