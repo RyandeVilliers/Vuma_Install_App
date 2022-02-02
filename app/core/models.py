bba2d2fd402fa337aad3d2cb1c00bef234b006e6
nirvaansingh47 <nirvaansingh47@gmail.com>
2022-02-02 10:53:43 +0200

    Adds migrations to project
    Adds migrations to project

--- app/core/models.py
+++ app/core/models.py
@@ -1,15 +1,17 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
-                                        PermissionsMixin
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 from django.conf import settings
 
 
 class UserManager(BaseUserManager):
-
     def create_user(self, email, password=None, **extra_fields):
         """Creates and saves a new user"""
         if not email:
-            raise ValueError('Users must have an email address')
+            raise ValueError("Users must have an email address")
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
@@ -28,46 +30,53 @@ class UserManager(BaseUserManager):
 
 class User(AbstractBaseUser, PermissionsMixin):
     """Custom user model that supports using email instead of username"""
+
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     objects = UserManager()
 
-# Customise username email form username to email
-    USERNAME_FIELD = 'email'
+    # Customise username email form username to email
+    USERNAME_FIELD = "email"
 
-
-class Status(models.Model):
-    """Status to be used to track progress of installs"""
-    status = models.CharField(max_length=255)
-    notes = models.CharField(max_length=255, blank=True)
-    date = models.DateField(auto_now=True)
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-        )
-
-    def __str__(self):
-        return self.status
 
 class Installation(models.Model):
     """Installation object"""
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE
-    )
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     customer_name = models.CharField(max_length=255)
-    address=models.CharField(max_length=255)
-    appointment_date=models.DateField(auto_created=True, null=True)
+    address = models.CharField(max_length=255)
+    appointment_date = models.DateField(auto_created=True, null=True)
     date_created = models.DateField(auto_now_add=True)
-    status = models.ForeignKey(
-        settings.STATUS_MODEL,
-        on_delete=models.PROTECT,
-        null=True
-    )
     date_modified = models.DateField(auto_now=True)
 
     def __str__(self):
         return self.customer_name
 
+
+STATUS_CHOICES = (
+    ("Installation Requested", "Installation Requested"),
+    ("Installation in Progress", "Installation in Progress"),
+    ("Installation Rejected", "Installation Rejected"),
+)
+
+
+class Status(models.Model):
+    """Status to be used to track progress of installs"""
+
+    status = models.CharField(
+        max_length=255, default=STATUS_CHOICES[0], choices=STATUS_CHOICES
+    )
+    notes = models.CharField(max_length=255, blank=True)
+    date = models.DateField(auto_now=True)
+    install = models.ForeignKey(
+        Installation,
+        related_name="status",
+        on_delete=models.CASCADE,
+        default=None,
+        null=True,
+    )
+
+    def __str__(self):
+        return self.status
