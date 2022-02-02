76def8ecf4b4ba90c06d0f4ef921a063151fd02b
RyandeVilliers <ryandv62@gmail.com>
2022-02-02 09:42:54 +0200

    Add feature for listing installations

--- app/installations/serializers.py
+++ app/installations/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from core.models import Status
+from core.models import Status, Installation
 
 class StatusSerializer(serializers.ModelSerializer):
     """Serializer for status objects"""
@@ -9,3 +9,17 @@ class StatusSerializer(serializers.ModelSerializer):
         model = Status
         fields = ('id', 'status', 'notes', 'date', 'user')
         read_only_fields = ('id',)
+
+
+class InstallationSerializer(serializers.ModelSerializer):
+    """Serialize an installation"""
+    status = serializers.PrimaryKeyRelatedField(
+        many=False,
+        queryset=Installation.objects.all()
+    )
+
+    class Meta:
+        model = Installation
+        fields = ('id', 'customer_name', 'address', 'appointment_date', 'date_created', 
+                    'date_modified', 'status')
+        read_only_fields = ('id')
